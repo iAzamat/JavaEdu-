1e7e83f4fede54ce3e5876787c8647a3f2e8f0f3
iAzamat <isabirovazamati@gmail.com>
2022-11-27 12:25:34 +0500

    Добавлен код из 3 лекции

---
+++ Lections/Java03/Ex002.java
@@ -0,0 +1,17 @@
+package Lections.Java03;
+import java.util.ArrayList;
+public class Ex002 {
+
+   public static void main(String[] args) {
+       ArrayList<Integer> list = new ArrayList<Integer>();
+       //ArrayList list = new ArrayList();
+       list.add(2809);
+//       list.add("String line");
+
+
+       for (Object o : list) {
+           System.out.println(o);
+           // Проблема извлечения данных
+       }
+   }
+}
